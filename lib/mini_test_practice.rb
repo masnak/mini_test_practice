0c6e3de55746b031a415b6378593ec124f88738f
Masamichi Nakamura <masamichi.nakamura@gmail.com>
2015-07-12 05:18:25 +0000

    add methods

--- lib/mini_test_practice.rb
+++ lib/mini_test_practice.rb
@@ -2,12 +2,17 @@ require "mini_test_practice/version"
 
 module MiniTestPractice
   class Main
-    def odd?(n)
-      if n == 1 || n == 3
-        return true
-      else
-        return false
-      end
+    def odd?(value)
+      (value % 2) == 1
     end
+
+    def check_number?(value)
+      (value.abs.to_s.size) >= 4 && (value % 2) == 0
+    end
+
+    def enough_length?(value)
+      (value.to_s.size) > 2 && (value.to_s.size) < 9
+    end
+
   end
 end
--- test/mini_test_practice_test.rb
+++ test/mini_test_practice_test.rb
@@ -10,9 +10,24 @@ class MiniTestPracticeTest < Minitest::Test
     refute_nil ::MiniTestPractice::VERSION
   end
 
-  def test_odd?
-    assert @main.odd?(1), '1 is odd'
-    refute @main.odd?(2), '2 is not odd'
-    assert @main.odd?(3), '3 is odd'
+    def test_odd?
+    assert_equal false, @main.odd?(0)
+    assert_equal true,  @main.odd?(1)
+    assert_equal false, @main.odd?(2)
+    assert_equal true,  @main.odd?(3)
   end
+
+  def test_check_number?
+    assert_equal true,  @main.check_number?(4000)
+    assert_equal false, @main.check_number?(4001)
+    assert_equal false, @main.check_number?(0000)
+    assert_equal false, @main.check_number?(302)
+  end
+
+  def test_enough_length?
+    assert_equal true, @main.enough_length?(400)
+  end
+
+
+
 end
